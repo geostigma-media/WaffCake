5025656cc2da276a69dc89b0ef7a45dc96460ca6
Andres Urrea <landresul@gmail.com>
2020-10-30 00:46:52 -0500

    Add notification for new accounts created from admin dashboard

---
+++ app/Notifications/NewUserAccountNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\HtmlString;
+
+class NewUserAccountNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->greeting('Bienvenido!')
+                    ->subject('Una cuenta ha sido creada - Waff Cake')
+                    ->line('Estás registrado en nuestro programa de beneficios Waffcake, utiliza el botón para completar tu registro:')
+                    ->action('Completar registro', url('/'))
+                    ->line('Conoce tu código y comienza a disfrutar del sabor wafflero con descuentos especiales. Recuerda que lo activas con tu primera compra.')
+                    ->line(new HtmlString('<br/>'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
